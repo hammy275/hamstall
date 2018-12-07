31df4219676d368904076985b2b5ff212281f87c
hammy3502 <hammy275@gmail.com>
2018-12-07 20:09:58 +0000

    More info in README & some code overhauling

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # hamstall
-A very small package manager that really shouldn't be used (it's more of a "see if I can design this" king of thing).
+A very small package manager that really shouldn't be used (it's more of a "see if I can do this" king of thing).
 
-##If you really want to use it
+## If you really want to use it
 At the moment, this program only supports the installing and uninstalling of .tar.gz files.
 Not much sanity checking takes place and this is very much in "beta" (even though this really shouldn't be used anyways)
 
-##Installation
-Download this repo, then run hamstall.py with Python3!
+## Installation
+Download this repo, then run hamstall.py with Python3! You will need to run -first when you run it for the first time to prepare your system.
+
+## Important info
+This will be updated, and until I mention otherwise on this page, there will be no backwards compatability. Stuff will break between some updates.
--- hamstall.py
+++ hamstall.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 #Hamstall
-#Please don't use this in an actual environment, it isn't intended for that at all.
+#Please don't use this in an actual environment. It doesn't have much sanity checking, and I update it a lot, breaking compatability with older versions.
 
 import os
 import argparse
@@ -9,45 +9,58 @@ from pathlib import Path
 import sys
 import re #Imports that are needed
 
-def GetInternalName(program): #Get name of program as it's stored
-    program_internal_name = re.sub(r'.*/', '/', program) #Remove a lot of stuff (I'm going to pretend I know how re.sub works, but this gives you "/filename.tar.gz"
-    program_internal_name = program_internal_name[1:(len(program_internal_name)-7)] #Some Python formatting that turns the path into "tarname"
-    return program_internal_name #Return internal name (tarname)
+class File:
+    def internalName(program): #Get name of program as it's stored
+        program_internal_name = re.sub(r'.*/', '/', program) #Remove a lot of stuff (I'm going to pretend I know how re.sub works, but this gives you "/filename.tar.gz"
+        program_internal_name = program_internal_name[1:(len(program_internal_name)-7)] #Some Python formatting that turns the path into "tarname"
+        return program_internal_name #Return internal name (tarname)
+
+    def extension(program):
+        return program[((len(program))-7):len(program)] #Returns the last 7 characters of the provided file name.
+
+    def exists(file_name):
+        return os.path.isfile(os.path.expanduser(file_name)) #Returns True if the given file exists. Otherwise false.
 
 def Install(program):
+    program_internal_name = File.internalName(program)
     print("Removing old temp directory (if it exists!)")
     os.system("rm -rf ~/.hamstall/temp") #Removes temp directory (used during installs)
     print("Creating new temp directory")
     os.system("mkdir ~/.hamstall/temp") #Creates temp directory for extracting tar
     print("Extracting tar to temp directory")
-    command_to_go = "tar xvzf " + program + " -C ~/.hamstall/temp/"
+    file_extension = File.extension(program)
+    if file_extension == '.tar.gz':
+        command_to_go = "tar xf " + program + " -C ~/.hamstall/temp/"
+    elif file_extension == '.tar.xz':
+        command_to_go = "tar xf " + program + " -C ~/.hamstall/temp/"
     os.system(command_to_go) #Extracts program tar
     print("Moving program to directory")
-    program_internal_name = GetInternalName(program)
-##    print(program_internal_name)
     os.system("mkdir ~/.hamstall/bin/" + program_internal_name) #Makes directory for program
     os.system("mv ~/.hamstall/temp/" + program_internal_name + " ~/.hamstall/bin/" ) #Moves program files
     print("Adding program to hamstall list of programs")
-    os.system('echo "rm -rf ~/.hamstall/bin/' + program_internal_name + '" > ~/.hamstall/uninstall_scripts/' + program_internal_name + '.sh') #Creates uninstall script
-    os.system('chmod +x ~/.hamstall/uninstall_scripts/' + program_internal_name + '.sh') #adds line to uninstall script to remove it
+    os.system('echo "rm -rf ~/.hamstall/bin/' + program_internal_name + '" > ~/.hamstall/uninstall_scripts/' + program_internal_name) #Creates uninstall script
+    os.system('chmod +x ~/.hamstall/uninstall_scripts/' + program_internal_name) #adds line to uninstall script to remove it
     #Figure out path stuff
     print("Install Completed!")
 
 def Uninstall(program):
     print("Removing program files")
-    os.system("~/.hamstall/uninstall_scripts/" + program + ".sh") #Runs uninstall script
+    os.system("~/.hamstall/uninstall_scripts/" + program) #Runs uninstall script
     print("Removing uninstall script")
-    os.system("rm -rf ~/.hamstall/uninstall_scripts/" + program + ".sh") #Removes uninstall script
+    os.system("rm -rf ~/.hamstall/uninstall_scripts/" + program) #Removes uninstall script
     print("Uninstall complete!")
 
 def ListPrograms():
     os.system("ls ~/.hamstall/uninstall_scripts")
     #Will put a better thing in here at some point
+
 def FirstTimeSetup():
+    print("Creating directories")
     os.system("mkdir ~/.hamstall")
     os.system("mkdir ~/.hamstall/temp")
     os.system("mkdir ~/.hamstall/bin")
-    os.system("mkdir ~/.hamstall/uninstall_scripts")
+    os.system("mkdir ~/.hamstall/uninstall_scripts") #Create some directories
+    print('First time setup complete!')
 
 def get_input(question, options, default): #Like input but with some checking
     answer = "-1" #Set answer to something
@@ -59,6 +72,9 @@ def get_input(question, options, default): #Like input but with some checking
     else:
         return answer #Return answer if it isn't the default answer
 
+###############Argument Parsing Below###############
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-install", help="Install a .tar.gz")
 parser.add_argument("-uninstall", help="Uninstall an insatlled program")
@@ -67,12 +83,12 @@ parser.add_argument("-first", help="Run first time setup")
 args = parser.parse_args() #Parser stuff
 if args.install != None:
     to_install = args.install #to_install from the argument
-    does_tar_exist = os.path.isfile(os.path.expanduser(to_install))
+    does_tar_exist = File.exists(to_install)
     if does_tar_exist == False:
         print("File to install does not exist!")
         sys.exit()
-    program_internal_name = GetInternalName(to_install) #Get the internal name (trim off everything except the file name before the .tar.gz
-    file_check = os.path.isfile(os.path.expanduser("~/.hamstall/uninstall_scripts/" + program_internal_name + ".sh")) #Checks to see if the file path for the program's uninstall script exists
+    program_internal_name = File.internalName(to_install) #Get the internal name (trim off everything except the file name before the .tar.gz
+    file_check = File.exists("~/.hamstall/uninstall_scripts/" + program_internal_name) #Checks to see if the file path for the program's uninstall script exists
     if file_check == True: #Uninstall script exists, ask to reinstall program
         reinstall = get_input("Application already exists! Would you like to reinstall? [y/N]", ["y", "n"], "n") #Ask to reinstall
         if reinstall == "y":
@@ -85,7 +101,7 @@ if args.install != None:
 
 elif args.uninstall != None:
     to_uninstall = args.uninstall
-    file_check = os.path.isfile(os.path.expanduser("~/.hamstall/uninstall_scripts/" + to_uninstall + ".sh")) #Checks to see if the file path for the program's uninstall script exists
+    file_check = File.exists("~/.hamstall/uninstall_scripts/" + to_uninstall) #Checks to see if the file path for the program's uninstall script exists
     if file_check == True: #If uninstall script exists
         Uninstall(to_uninstall) #Uninstall program
     else:
@@ -94,4 +110,4 @@ elif args.list != None:
     ListPrograms() #List programs installed
 
 elif args.first != None:
-    FirstTimeSetup()
+    FirstTimeSetup() #First time setup

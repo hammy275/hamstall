f611eeff8e240cb364b60c8a3c55405b7acfc8ce
hammy3502 <hammy3502>
2018-12-09 00:25:02 -0500

    Minor backend changes to make things easier and neater.

--- hamstall.py
+++ hamstall.py
@@ -9,8 +9,8 @@ from pathlib import Path
 import sys
 import re #Imports that are needed
 
-class File:
-    def internalName(program): #Get name of program as it's stored
+class file:
+    def name(program): #Get name of program as it's stored
         program_internal_name = re.sub(r'.*/', '/', program) #Remove a lot of stuff (I'm going to pretend I know how re.sub works, but this gives you "/filename.tar.gz"
         program_internal_name = program_internal_name[1:(len(program_internal_name)-7)] #Some Python formatting that turns the path into "tarname"
         return program_internal_name #Return internal name (tarname)
@@ -22,13 +22,13 @@ class File:
         return os.path.isfile(os.path.expanduser(file_name)) #Returns True if the given file exists. Otherwise false.
 
 def Install(program):
-    program_internal_name = File.internalName(program)
+    program_internal_name = file.name(program)
     print("Removing old temp directory (if it exists!)")
     os.system("rm -rf ~/.hamstall/temp") #Removes temp directory (used during installs)
     print("Creating new temp directory")
     os.system("mkdir ~/.hamstall/temp") #Creates temp directory for extracting tar
     print("Extracting tar to temp directory")
-    file_extension = File.extension(program)
+    file_extension = file.extension(program)
     if file_extension == '.tar.gz':
         command_to_go = "tar xf " + program + " -C ~/.hamstall/temp/"
     elif file_extension == '.tar.xz':
@@ -40,7 +40,7 @@ def Install(program):
     print("Adding program to hamstall list of programs")
     os.system('echo "rm -rf ~/.hamstall/bin/' + program_internal_name + '" > ~/.hamstall/uninstall_scripts/' + program_internal_name) #Creates uninstall script
     os.system('chmod +x ~/.hamstall/uninstall_scripts/' + program_internal_name) #adds line to uninstall script to remove it
-    #Figure out path stuff
+    #Here is where I should add stuff to PATH.
     print("Install Completed!")
 
 def Uninstall(program):
@@ -52,7 +52,7 @@ def Uninstall(program):
 
 def ListPrograms():
     os.system("ls ~/.hamstall/uninstall_scripts")
-    #Will put a better thing in here at some point
+    #This actually works pretty well. May make something better sooner or later.
 
 def FirstTimeSetup():
     print("Creating directories")
@@ -76,19 +76,19 @@ def get_input(question, options, default): #Like input but with some checking
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-install", help="Install a .tar.gz")
+parser.add_argument("-install", help="Install a .tar.gz or .tar.xz")
 parser.add_argument("-uninstall", help="Uninstall an insatlled program")
 parser.add_argument("-list", help="List installed programs")
 parser.add_argument("-first", help="Run first time setup")
 args = parser.parse_args() #Parser stuff
 if args.install != None:
     to_install = args.install #to_install from the argument
-    does_tar_exist = File.exists(to_install)
+    does_tar_exist = file.exists(to_install)
     if does_tar_exist == False:
         print("File to install does not exist!")
         sys.exit()
-    program_internal_name = File.internalName(to_install) #Get the internal name (trim off everything except the file name before the .tar.gz
-    file_check = File.exists("~/.hamstall/uninstall_scripts/" + program_internal_name) #Checks to see if the file path for the program's uninstall script exists
+    program_internal_name = file.name(to_install) #Get the internal name (trim off everything except the file name before the .tar.gz
+    file_check = file.exists("~/.hamstall/uninstall_scripts/" + program_internal_name) #Checks to see if the file path for the program's uninstall script exists
     if file_check == True: #Uninstall script exists, ask to reinstall program
         reinstall = get_input("Application already exists! Would you like to reinstall? [y/N]", ["y", "n"], "n") #Ask to reinstall
         if reinstall == "y":
@@ -101,11 +101,12 @@ if args.install != None:
 
 elif args.uninstall != None:
     to_uninstall = args.uninstall
-    file_check = File.exists("~/.hamstall/uninstall_scripts/" + to_uninstall) #Checks to see if the file path for the program's uninstall script exists
+    file_check = file.exists("~/.hamstall/uninstall_scripts/" + to_uninstall) #Checks to see if the file path for the program's uninstall script exists
     if file_check == True: #If uninstall script exists
         Uninstall(to_uninstall) #Uninstall program
     else:
         print("Program does not exist!") #Program doesn't exist
+
 elif args.list != None:
     ListPrograms() #List programs installed
 
